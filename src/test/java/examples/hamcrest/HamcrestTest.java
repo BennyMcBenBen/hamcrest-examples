7d4fd88107edd2e85f6b3d509f6761a240ada235
Ben Zoller <benjamin.zoller@gmail.com>
2014-07-08 09:23:01 -0400

    Added remaining non-XML tests

--- src/test/java/examples/hamcrest/HamcrestTest.java
+++ src/test/java/examples/hamcrest/HamcrestTest.java
@@ -1,15 +1,19 @@
 package examples.hamcrest;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang3.ArrayUtils;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -69,6 +73,8 @@ public class HamcrestTest {
         assertThat(2 + 2, not(lessThan(4)));
 
         assertThat("foo", comparesEqualTo("foo"));
+
+        assertThat("foo", isOneOf("bar", "foo", "baz"));
     }
 
     @Test
@@ -77,6 +83,8 @@ public class HamcrestTest {
         Object o2 = new Object();
         assertThat(o1, sameInstance(o1));
         assertThat(o1, not(sameInstance(o2)));
+
+        assertThat(o1, is(theInstance(o1)));
     }
 
     @Test
@@ -90,7 +98,7 @@ public class HamcrestTest {
     }
 
     @Test
-    public void integerComparisons() {
+    public void comparables() {
         assertThat(1, lessThan(2));
         assertThat(1, lessThanOrEqualTo(1));
         assertThat(1, equalTo(1));
@@ -104,10 +112,17 @@ public class HamcrestTest {
     }
 
     @Test
+    public void bigDecimalComparisons() {
+        assertThat(new BigDecimal(3.14), closeTo(new BigDecimal(3.15),new BigDecimal(.01)));
+    }
+
+    @Test
     public void testToString() {
         List list = ImmutableList.of(1, 2, 3);
         assertThat("testing toString is often a code smell, but there is a matcher for it just in case",
                 list, hasToString("[1, 2, 3]"));
+
+        assertThat(list, hasToString(containsString("2")));
     }
 
     @Test
@@ -125,6 +140,8 @@ public class HamcrestTest {
 
         assertThat("foo", startsWith("f"));
         assertThat("foo", endsWith("o"));
+
+        assertThat("abcdefghi", containsString("def"));
 
         assertThat("foo bar baz qux", stringContainsInOrder(ImmutableList.of("foo", "baz", "qux")));
     }
@@ -163,9 +180,15 @@ public class HamcrestTest {
 
         assertThat(a, arrayContaining(1, 2, 3));
         assertThat(a, not(arrayContaining(3, 2, 1)));
+        assertThat(a, arrayContaining(is(1), is(2), is(3)));
+        assertThat(a, arrayContaining(Arrays.<Matcher<? super Integer>>asList(is(1), is(2), is(3))));
 
         assertThat(a, arrayContainingInAnyOrder(1, 2, 3));
         assertThat(a, arrayContainingInAnyOrder(3, 2, 1));
+        assertThat(a, arrayContainingInAnyOrder(is(3), is(1), is(2)));
+        assertThat(a, arrayContainingInAnyOrder(Arrays.<Matcher<? super Integer>>asList(is(3), is(1), is(2))));
+
+        assertThat(2, isIn(a));
 
         // What about primitive arrays? Use Commons Lang ArrayUtils.
         int[] primitives = {1, 2, 3};
@@ -174,35 +197,61 @@ public class HamcrestTest {
 
     @Test
     public void testCollections() {
-        // TODO
+        assertThat(new ArrayList<String>(), is(empty()));
+        assertThat(new ArrayList<String>(), is(emptyCollectionOf(String.class)));
+
+        Collection<String> a = ImmutableList.of("foo", "bar", "baz");
+        assertThat(a, hasSize(3));
+        assertThat(a, hasSize(is(3)));
+
+        assertThat("foo", isIn(a));
     }
 
     @Test
     public void testIterables() {
+        assertThat(new ArrayList<String>(), is(emptyIterable()));
+        assertThat(new ArrayList<String>(), is(emptyIterableOf(String.class)));
+
         Iterable<Integer> a = ImmutableList.of(1, 2, 3);
 
         assertThat(a, Matchers.<Integer>iterableWithSize(3));
         assertThat(a, Matchers.<Integer>iterableWithSize(is(3)));
 
         assertThat(a, contains(1, 2, 3));
-        assertThat(a, contains(is(1), is(2), is(3)));
-
         assertThat("single item collection", ImmutableList.of(1), contains(is(1)));
+        assertThat(a, contains(is(1), is(2), is(3)));
+        assertThat(a, contains(Arrays.<Matcher<? super Integer>>asList(is(1), is(2), is(3))));
 
         assertThat(a, containsInAnyOrder(3, 1, 2));
         assertThat(a, containsInAnyOrder(is(3), is(1), is(2)));
+        assertThat(a, containsInAnyOrder(Arrays.<Matcher<? super Integer>>asList(is(3), is(1), is(2))));
 
-        // TODO
+        assertThat(a, hasItem(2));
+        assertThat(a, hasItem(is(2)));
+
+        assertThat(a, hasItems(3, 2));
+        assertThat(a, hasItems(is(3), is(2)));
+
+        assertThat(Arrays.asList("bar", "baz"), everyItem(startsWith("ba")));
     }
 
     @Test
     public void testMaps() {
-        // TODO
+        Map<String, String> m = ImmutableMap.<String, String>builder().put("key1", "value1").put("key2", "value2").build();
+
+        assertThat(m, hasEntry("key1", "value1"));
+        assertThat(m, hasEntry(startsWith("k"), endsWith("1")));
+
+        assertThat(m, hasKey("key1"));
+        assertThat(m, hasKey(startsWith("k")));
+
+        assertThat(m, hasValue("value1"));
+        assertThat(m, hasValue(endsWith("1")));
     }
 
     @Test
     public void testXml() {
-        // TODO
+        // TODO hasXPath
     }
 
     @Test
